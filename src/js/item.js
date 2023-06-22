2298932859ed9501c3c9a17f4a590c1a025c08d3
ellehailu <hailu.elle@gmail.com>
2023-06-22 13:42:45 -0700

    pass test for creating an item object

--- src/js/item.js
+++ src/js/item.js
@@ -1,5 +1,7 @@
 export default class Item {
     constructor(name, monetaryValue, connectionPoint){
-        
+        this.name = name;
+        this.monetaryValue = monetaryValue;
+        this.connectionPoint = connectionPoint;
     }
 }
